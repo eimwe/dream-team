e21f1d4e5f1dc530a131272cc8eaad32ad3b2f58
eimwe <prokopenkoserge@gmail.com>
2026-07-18 13:02:38 +0500

    Performed db migration for user-status relationship check

---
+++ Migrations/20260718075710_CheckUserStatusRelationship.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dream_team.Migrations
+{
+    /// <inheritdoc />
+    public partial class CheckUserStatusRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
