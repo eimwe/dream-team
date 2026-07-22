48245026559929191e9ce188328966979f6dacbb
eimwe <prokopenkoserge@gmail.com>
2026-07-22 21:56:11 +0500

    Updated auth view model

--- Models/ViewModels/AuthViewModel.cs
+++ Models/ViewModels/AuthViewModel.cs
@@ -2,6 +2,9 @@ namespace dream_team.Models.ViewModels;
 
 public class AuthViewModel
 {
+    public string? Login { get; set; }
+    public string? Password { get; set; }
+
     public string? ErrorMessage { get; set; }
     public string? SuccessMessage { get; set; }
 }
